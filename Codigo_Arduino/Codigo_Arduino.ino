8a7c8934fe66a7abcf1b48f0471f8c02fdae7778
OctavioHaeublein <Octavio.Haeublein41@gmail.com>
2019-08-26 17:11:44 -0300

    Agregado archivos
    Agregado de script de python para comenzar, y el codigo de Arduino modificado.

--- Codigo_Arduino/Codigo_Arduino.ino
+++ Codigo_Arduino/Codigo_Arduino.ino
@@ -4,35 +4,57 @@
 #include <SPI.h>
 #include <EEPROM.h>
 
+#define PI 3.1415926535897932384626433832795
+
 /*---VARIABLES MODULO SD---*/
 int cs = 53;
+int posicion_principal = 0;
+int posicion_secundario = 0;
+String datos_principal = "datos_principal_";
+String datos_secundario = "datos_secundario_";
 
 /*---VARIABLES SENSORES---*/
-int frecuencia_rueda = 0;
+float revoluciones = 0;
 float tension_divisor = 0;
 float tension_hall = 0;
 
 
 /*---VARIABLES PROGRAMA---*/
 float tiempo = 0;               //Tiempo transcurrido del programa en horas
+float tiempo_inicio = 0;
 float tiempo_actual = 0;        //Variable auxiliar para el calculo del tiempo
 float tiempo_previo = 0;        //Variable auxiliar para el calculo del tiempo
 
 String archivo = "";            //Nombre del archivo al que deben ser guardados los datos en la tarjeta SD
-int muestreo = 1000;            //Frecuencia con la que se almacenan los datos en la tarjeta SD
+int muestreo = 1000;            //Frecuencia con la que se almacenan los datos en la tarjeta SD (ms)
 bool doble = false;             //Booleano que comprueba si los datos deberan guardarse en un archivo a parte
 
 float tension = 0;              //Nivel de tension de las baterias
 float corriente = 0;            //Corriente calculada del motor (Depende si carga, o no lo hace)
 float potencia = 0;             //Potencia del motor
 float energia = 0;              //Energia consumida por el motor
-float velocidad = 0;            //Velocidad (Km/h) del vehículo
-//bool carga = false;             //Booleano que indica el estado de carga de las baterias
+
+float frecuencia = 0;           //Revoluciones por segundo del eje
+float diametro = 0;             //Diametro del eje (mm)
+float velocidad = 0;            //Velocidad tangencial de la rueda (Km/h)
+
+bool modo_debug = true;
+bool constantes = true;
+
+/*---ERRORES---*/
+String error = "";
+
+/*
+ * ERROR 1 -> ERROR COMUNICANDO CON EL MODULO SD
+ * ERROR 2 -> ERROR ABRIENDO ARCHIVO ESPECIFICADO EN SD
+ * ERROR 3 -> ERROR LEYENDO CONSTANTES DESDE ARCHIVO
+*/
 
 void setup() {
   Serial.begin(9600);           //Inicia una comunicacion serial para edicion de codigo
-  tiempo_actual = millis();     //Toma el tiempo de inicio del programa en milisegundos
+  tiempo_inicio = millis();     //Toma el tiempo de inicio del programa en milisegundos
   attachInterrupt(digitalPinToInterrupt(2), blink, LOW);
+  attachInterrupt(digitalPinToInterrupt(3), blink, LOW);
   
   /*---DIGITAL OUTPUTS---*/
   pinMode(23,OUTPUT);   //LED Rojo1
@@ -58,43 +80,48 @@ void setup() {
   /*---ANALOG INPUTS---*/
   pinMode(A0,INPUT);    //Nivel de tension de las baterias
   pinMode(A1, INPUT);   //Nivel de tension del sensor Hall
+
+  /*---LECTURA DE CONSTANTES EN SD---*/
+  
   
 }
 
 void loop() {
   /*---CALCULO DE TIEMPO---*/
-  tiempo_previo = tiempo_actual;
   tiempo_actual = millis();
-  tiempo = ((tiempo_actual-tiempo_previo)/3600000);      //Calcula el tiempo transcurrido en horas para la energia
-  /*---LECTURA DE SENSORES---*/
-  tension_divisor = analogRead(A0);
-  tension_hall = analogRead(A1);
-
-  /*---CALCULO DE PARAMETROS---*/
-  tension = map(tension_divisor, 0,1023,0,60);  //Mapea los niveles de tension recibidos de 0 a 1023, para 0 a 60v
-  
-  if(tension_hall >= 511.5){
-    
-    corriente = map(tension_hall, 511.5 ,1023, 0, 50);
-    //carga = false;
-    
-  }else{if(tension_hall < 2.5){
-    
-      corriente = -(map(tension_hall, 511.5, 1023, 0, 50));
-      //carga = true;   
-    }
-   }
-
-  potencia = (tension * corriente);
-  energia += (potencia * tiempo);
+  tiempo = ((tiempo_actual-tiempo_inicio)/3600000);      //Calcula el tiempo transcurrido en horas para la energia
 
   /*---COMPROBACION DE TIEMPO---*/
   if((tiempo_actual - tiempo_previo) >= muestreo){
-    SD_guardar(tiempo, tension, corriente, potencia, energia, velocidad, archivo);
+    
+    tiempo_previo = tiempo_actual;
+    
+    detachInterrupt(digitalPinToInterrupt(2));                //Elimina temporalmente las interrupciones para poder grabar los datos
+    detachInterrupt(digitalPinToInterrupt(3));
+    
+    calcular();
+    
+    datos_principal.concat(posicion_principal);
+    if(modo_debug){debug();}
+    SD_guardar(datos_principal);
+    datos_principal = "datos_principal_";
+    posicion_principal++;
+    
     if(doble){
-      SD_guardar(tiempo, tension, corriente, potencia, energia, velocidad, archivo);
+      datos_secundario.concat(posicion_secundario);
+      SD_guardar(datos_secundario);
+      datos_secundario = "datos_secundario_";
+      posicion_secundario++;
     }
+    
+    attachInterrupt(digitalPinToInterrupt(2), blink, LOW);    //Vuelve a habilitar las interrupciones para que funcione el programa
+    attachInterrupt(digitalPinToInterrupt(3), blink, LOW);
   }
+
+  if(!constantes){
+    SD_constantes();
+  }
+  
 }
 
 void LCD_datos (float tension,float corriente, float potencia, float energia, float velocidad){
@@ -119,21 +146,21 @@ void LCD_error (String error){
   
 }
 
-void SD_guardar (float tiempo, float tension,float corriente, float potencia, float energia, float velocidad, String archivo){
+void SD_guardar (String archivo){
   
   File datos;                             //Crea el objeto "datos" para trabajar con la librería
   
   if(!SD.begin(cs)){
-    Serial.println("Error conectando con SD");
-    LCD_error("Error conectando con SD");
+    Serial.println("E1");
+    LCD_error("E1");
     return;
   }
   
   datos = SD.open(archivo,FILE_WRITE);    //La variable "Archivo" Puede variar entre copias y el principal
   
   if(!datos){                             //Comprueba si puede escribir a la SD, envía error si no fuese así
-    Serial.println("Error escribiendo a SD");
-    LCD_error("Error escribiendo a SD");
+    Serial.println("E2");
+    LCD_error("E2");
     return;
   }
                                           //Almacena las distintas variables separadas por una coma
@@ -153,6 +180,79 @@ void SD_guardar (float tiempo, float tension,float corriente, float potencia, fl
   return;
 }
 
+void SD_constantes(){
+  
+ if(SD.begin(cs)){
+  
+    File archivo;
+    archivo = SD.open("constantes.txt");
+    if(archivo){
+      archivo.seek(0);
+      posicion_principal = archivo.read();
+      posicion_secundario = archivo.read();
+      constantes = true;
+      archivo.close();
+    }else{
+      LCD_error("E3");
+    }
+  }else{
+    LCD_error("E3");
+  }
+  
+}
+float calcular (){
+  /*---LECTURA DE SENSORES---*/
+  tension_divisor = analogRead(A0);
+  tension_hall = analogRead(A1);
+
+  /*---CALCULO DE TENSION, CORRIENTE, POTENCIA Y ENERGIA---*/
+  tension = map(tension_divisor, 0,1023,0,60);  //Mapea los niveles de tension recibidos de 0 a 1023, para 0 a 60v
+  
+  if(tension_hall >= 511.5){
+    
+    corriente = map(tension_hall, 511.5 ,1023, 0, 50);
+    
+  }else{if(tension_hall < 2.5){
+    
+      corriente = -(map(tension_hall, 511.5, 1023, 0, 50));
+      
+    }
+   }
+
+  potencia = (tension * corriente);
+  energia += (potencia * tiempo);
+
+  /*---CALCULO DE VELOCIDAD TANGENCIAL---*/
+  frecuencia = (revoluciones/((tiempo_actual-tiempo_previo)/1000));
+  revoluciones = 0;
+  velocidad = ((PI*diametro*frecuencia)/1000);
+  
+  return tension, corriente, potencia, energia, velocidad;
+}
+
+void debug (){
+  Serial.print("Tiempo transcurrido: ");
+  Serial.println(tiempo);
+  Serial.print("Tension: ");
+  Serial.println(tension);
+  Serial.print("Corriente: ");
+  Serial.println(corriente);
+  Serial.print("Potencia: ");
+  Serial.println(potencia);
+  Serial.print("Energia: ");
+  Serial.println(energia);
+  Serial.print("Velocidad: ");
+  Serial.println(velocidad);
+  Serial.println(datos_principal);
+  Serial.println(datos_secundario);
+  Serial.println(error);
+}
+
 void blink(){
-  doble = !doble;
+  if(digitalRead(2)==0){
+    doble = !doble;
+  }else{
+    revoluciones++;
+  }
+
 }
---
+++ Main.py
@@ -0,0 +1,20 @@
+print("----------------------------------------")
+print("1. Cargar datos")
+print("2. Graficar")
+print("3. Salir")
+print("----------------------------------------")
+entrada = input("Opción: ")
+
+if(entrada == "1"):
+    directorio = input("Introduzca el directorio del archivo: ")
+    archivo = input("Introduzca el nombre del archivo: ")
+if(entrada == "2"):
+    print("----------------------------------------")
+    print("1. Todo")
+    print("2. Velocidad")
+    print("3. Tensión")
+    print("4. Corriente")
+    print("5. Potencia")
+    print("6. Energía")
+    print("----------------------------------------")
+    entrada = input("opcion: ")
